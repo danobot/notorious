80ef19b17ff338fb0344af02e6896f26cce43bac
Daniel Mason <danielbaker210@gmail.com>
2020-03-21 22:54:44 +0800

    chore: check for updates periodically

--- app/AppUpdater.ts
+++ app/AppUpdater.ts
@@ -1,10 +1,15 @@
 import { autoUpdater } from "electron-updater"
+const log = require("electron-log")
 
 export default class AppUpdater {
   constructor() {
-    const log = require("electron-log")
+    console.log("AppUpdater Created")
     log.transports.file.level = "debug"
     autoUpdater.logger = log
     autoUpdater.checkForUpdatesAndNotify()
+    setInterval(() => {
+      autoUpdater.checkForUpdatesAndNotify();
+  }, 1000 * 60 * 15);
+    console.log("autoUpdater",autoUpdater)
   }
 }
--- app/store/configurePouchDB.ts
+++
@@ -1,5 +0,0 @@
-
-
-export const dbUrl = process.env.NODE_ENV === 'production'    ? 'http://admin:admin@localhost:5984/' : 'http://admin:admin@localhost:5984/'
-
-
