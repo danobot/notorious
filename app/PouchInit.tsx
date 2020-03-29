a817b908ea0d3725cba53bfe00024eef5473e512
Daniel Mason <danielbaker210@gmail.com>
2020-03-29 13:55:55 +0800

    fix: revert replication optimisation (first time sync fails)

--- app/PouchInit.tsx
+++ app/PouchInit.tsx
@@ -14,7 +14,6 @@ const remoteNotesDb = new PouchDB(`${config.db}/notes`);
 
 export default class PouchInit {
   sync;
-
   constructor() {
     log.transports.file.level = 'debug';
     PouchDB.logger = log;
@@ -26,104 +25,111 @@ export default class PouchInit {
     remoteNotesDb.info().then(function(info) {
       console.log('remoteNotesDb', info);
     });
-
-    notesDB.replicate
-      .from(remoteNotesDb) // one-time replicateion on start up
-      .on('complete', function(info) {
-        console.log('First-time sync completed: ', info);
-
-        // then two-way, continuous, retriable sync
-        notesDB
-          .sync(remoteNotesDb, opts)
-          .on('change', function(info) {
-            console.log('remoteNotesDb sync:     handle change', info);
-          })
-          .on('paused', function(err) {
-            console.log(
-              'remoteNotesDb sync:     replication paused (e.g. replication up to date, user went offline)',
-              err
-            );
-          })
-          .on('active', function() {
-            console.log(
-              'notesDB replicate sync:     resumed (e.g. new changes replicating, user went back online)'
-            );
-          })
-          .on('denied', function(err) {
-            console.log(
-              ' notesDB sync:     a document failed to replicate (e.g. due to permissions)',
-              err
-            );
-          })
-          .on('complete', function(info) {
-            console.log('notesDB sync:     handle complete', info);
-          })
-          .on('error', function(err) {
-            console.log('notesDB sync:     handle error', err);
-          });
+    // configDB.sync(remoteConfigDb, syncOpts).on('change', function(info) {
+    //   console.log('remoteConfigDb sync:     handle change', info);
+    // })
+    this.sync = notesDB.sync(remoteNotesDb, syncOpts).on('change', function(info) {
+        console.log('remoteNotesDb sync:     handle change', info);
       })
-      .on('error', e => {
-        console.log('First-time sync failed: ', e);
+      .on('paused', function(err) {
+        console.log(
+          'remoteNotesDb sync:     replication paused (e.g. replication up to date, user went offline)',
+          err
+        );
+      })
+      .on('active', function() {
+        console.log(
+          'notesDB replicate sync:     resumed (e.g. new changes replicating, user went back online)'
+        );
+      })
+      .on('denied', function(err) {
+        console.log(
+          ' notesDB sync:     a document failed to replicate (e.g. due to permissions)',
+          err
+        );
+      })
+      .on('complete', function(info) {
+        console.log('notesDB sync:     handle complete', info);
+      })
+      .on('error', function(err) {
+        console.log('notesDB sync:     handle error', err);
       });
 
 
-    // remoteNotesDb
-    //   .changes()
-    //   .on('change', function(info) {
-    //     console.log('remoteNotesDb handle change', info);
-    //   })
-    //   .on('paused', function(err) {
-    //     console.log(
-    //       'remoteDB replication paused (e.g. replication up to date, user went offline)',
-    //       err
-    //     );
-    //   })
-    //   .on('active', function() {
-    //     console.log(
-    //       'remoteDB replicate resumed (e.g. new changes replicating, user went back online)'
-    //     );
-    //   })
-    //   .on('denied', function(err) {
-    //     console.log(
-    //       'remoteDB a document failed to replicate (e.g. due to permissions)',
-    //       err
-    //     );
-    //   })
-    //   .on('complete', function(info) {
-    //     console.log('remoteDB handle complete', info);
-    //   })
-    //   .on('error', function(err) {
-    //     console.log('remoteDB handle error', err);
-    //   });
+    remoteNotesDb
+      .changes()
+      .on('change', function(info) {
+        console.log('remoteNotesDb handle change', info);
+      })
+      .on('paused', function(err) {
+        console.log(
+          'remoteDB replication paused (e.g. replication up to date, user went offline)',
+          err
+        );
+      })
+      .on('active', function() {
+        console.log(
+          'remoteDB replicate resumed (e.g. new changes replicating, user went back online)'
+        );
+      })
+      .on('denied', function(err) {
+        console.log(
+          'remoteDB a document failed to replicate (e.g. due to permissions)',
+          err
+        );
+      })
+      .on('complete', function(info) {
+        console.log('remoteDB handle complete', info);
+      })
+      .on('error', function(err) {
+        console.log('remoteDB handle error', err);
+      });
 
-    // notesDB
-    //   .changes()
-    //   .on('change', function(info) {
-    //     console.log('notesDB handle change', info);
-    //   })
-    //   .on('paused', function(err) {
-    //     console.log(
-    //       'notesDB replication paused (e.g. replication up to date, user went offline)',
-    //       err
-    //     );
-    //   })
-    //   .on('active', function() {
-    //     console.log(
-    //       'notesDB replicate resumed (e.g. new changes replicating, user went back online)'
-    //     );
-    //   })
-    //   .on('denied', function(err) {
-    //     console.log(
-    //       'notesDB a document failed to replicate (e.g. due to permissions)',
-    //       err
-    //     );
-    //   })
-    //   .on('complete', function(info) {
-    //     console.log('notesDB handle complete', info);
-    //   })
-    //   .on('error', function(err) {
-    //     console.log('notesDB handle error', err);
-    //   });
+
+      notesDB
+      .changes()
+      .on('change', function(info) {
+        console.log('notesDB handle change', info);
+      })
+      .on('paused', function(err) {
+        console.log(
+          'notesDB replication paused (e.g. replication up to date, user went offline)',
+          err
+        );
+      })
+      .on('active', function() {
+        console.log(
+          'notesDB replicate resumed (e.g. new changes replicating, user went back online)'
+        );
+      })
+      .on('denied', function(err) {
+        console.log(
+          'notesDB a document failed to replicate (e.g. due to permissions)',
+          err
+        );
+      })
+      .on('complete', function(info) {
+        console.log('notesDB handle complete', info);
+      })
+      .on('error', function(err) {
+        console.log('notesDB handle error', err);
+      });
   }
 }
 
+// Download all data from remote on first start - to be implemented later for better efficiency
+// https://pouchdb.com/api.html#sync
+// It is also possible to combine “one-way” replication and sync for performance reasons. When your PouchDB application starts up it could perform a one-off, one-way replication to completion and then initiate the two-way, continuous retryable sync:
+
+// var url = 'http://localhost:5984/mydb';
+// var opts = { live: true, retry: true };
+
+// // do one way, one-off sync from the server until completion
+// db.replicate.from(url).on('complete', function(info) {
+//   // then two-way, continuous, retriable sync
+//   db.sync(url, opts)
+//     .on('change', onSyncChange)
+//     .on('paused', onSyncPaused)
+//     .on('error', onSyncError);
+// }).on('error', onSyncError);
+// The above technique results in fewer HTTP requests being used and better performance than just using db.sync on its own.
