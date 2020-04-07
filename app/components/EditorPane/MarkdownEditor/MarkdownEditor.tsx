e2f619c25ec00aa4e6ff66620dc9648ef6bd4270
Daniel Mason <danielbaker210@gmail.com>
2020-04-07 08:19:39 +0800

    fix: compilation error

--- app/components/EditorPane/MarkdownEditor/MarkdownEditor.tsx
+++ app/components/EditorPane/MarkdownEditor/MarkdownEditor.tsx
@@ -7,26 +7,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {  } from "@fortawesome/free-regular-svg-icons";
 import {  } from "@fortawesome/free-solid-svg-icons";
 
-import 'codemirror/lib/codemirror';
+// import 'codemirror/lib/codemirror';
 
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/xml/xml';
-import 'codemirror/mode/markdown/markdown';
-import 'codemirror/mode/gfm/gfm';
+// import 'codemirror/mode/javascript/javascript';
+// import 'codemirror/mode/xml/xml';
+// import 'codemirror/mode/markdown/markdown';
+// import 'codemirror/mode/gfm/gfm';
 
-import 'hypermd/core';
-import 'hypermd/mode/hypermd';
+// import 'hypermd/core';
+// import 'hypermd/mode/hypermd';
 
-import 'hypermd/addon/hide-token';
-import 'hypermd/addon/cursor-debounce';
-import 'hypermd/addon/fold';
-import 'hypermd/addon/read-link';
-import 'hypermd/addon/click';
-import 'hypermd/addon/hover';
-import 'hypermd/addon/paste';
-import 'hypermd/addon/insert-file';
-import 'hypermd/addon/mode-loader';
-import 'hypermd/addon/table-align';
+// import 'hypermd/addon/hide-token';
+// import 'hypermd/addon/cursor-debounce';
+// import 'hypermd/addon/fold';
+// import 'hypermd/addon/read-link';
+// import 'hypermd/addon/click';
+// import 'hypermd/addon/hover';
+// import 'hypermd/addon/paste';
+// import 'hypermd/addon/insert-file';
+// import 'hypermd/addon/mode-loader';
+// import 'hypermd/addon/table-align';
 
 const handler = f => useCallback(debounce(f, 2000), []);
 
