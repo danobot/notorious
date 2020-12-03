c3bbf2264cffdeae27cd7c70fa075dce4ff5d362
Daniel Mason <danobot@users.noreply.github.com>
2020-12-03 17:08:09 +0800

    Create dev-setup-debian-ubuntu.sh

---
+++ scripts/dev-setup-debian-ubuntu.sh
@@ -0,0 +1,6 @@
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt update
+
+sudo apt install build-essentials yarn
